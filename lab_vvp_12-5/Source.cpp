95320444328cfa18d10ec0afc0796a4d8682a82f
HYPERPC <HYPERPC@DESKTOP-9FMUOVG>
2021-11-14 19:14:43 +0300

    Добавьте файлы проекта.

---
+++ lab_vvp_12-5/Source.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+#include <windows.h>
+#define YEAR 1900
+
+const char* color[] =
+{
+"белой",
+"голубой",
+"зелЄной",
+"красной",
+"жЄлтой",
+"белого",
+"голубого",
+"зелЄного",
+"жЄлтого",
+"красного"
+};
+
+const char* animal[] =
+{
+"крысы",
+"коровы",
+"тигра",
+"зайца",
+"дракона",
+"змеи",
+"лошади",
+"овцы",
+"обезь€ны",
+"курицы",
+"собаки",
+"свиньи"
+};
+
+void main()
+{
+	setlocale(LC_CTYPE, "Russian_Russia.1251");
+
+	do
+	{
+		system("cls");
+
+		int input, term = 0, count = 0, animalIndex = 0, colorIndex = 0;
+
+		printf("\n ¬ведите год с 1900-го: ");
+		scanf("%d", &input);
+
+		term = input - YEAR;
+		animalIndex = (input - YEAR) % 12;
+		colorIndex = (input - YEAR) % 10 / 2;
+
+		if (animalIndex > 1 && animalIndex < 5) colorIndex += 5;
+
+		printf("\n %d это год %s %s", input, color[colorIndex], animal[animalIndex]);
+
+		_getch();
+	} while (1);
+}
